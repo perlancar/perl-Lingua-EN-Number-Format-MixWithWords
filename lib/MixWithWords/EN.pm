9f0fc51218d1b83d1bcbd6a57138f5d7116f353d
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-09-21 17:54:28 +0700

    add alias package: MixWithWords::EN

---
+++ lib/MixWithWords/EN.pm
@@ -0,0 +1,9 @@
+package MixWithWords::EN;
+
+use Lingua::EN::Number::Format::MixWithWords;
+use Package::Rename qw(link_package);
+
+link_package "Lingua::EN::Number::Format::MixWithWords", "MixWithWords::EN";
+
+1;
+# ABSTRACT: Alias for Lingua::EN::Number::Format::MixWithWords
